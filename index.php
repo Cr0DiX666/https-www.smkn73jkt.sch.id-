adaa78235b6bd56299a083e1525bd1ecf115ab72
Cr0DiX666 <68939380+Cr0DiX666@users.noreply.github.com>
2024-02-20 16:32:04 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html> 
+   <html>
+      <script> 
+        alert("XSS! Cross Site Scriptings"); </script>
+      <script>
+       alert("Please Pacth Bug Sir!!!"); </script>
+       <script>
+        alert("Show My Nick: Cr0DiX666@H4×0r.iĐ"); </script>
+    <body>
+ </head>
+   <body>
+<title>Hacked Cr0DiX666@H4×0r.iĐ</title>
+<center>
+<a href="https://ibb.co/gVks9HS"><img src="https://i.ibb.co/gVks9HS/20240220-012214.png" alt="20240220-012214" border="0"></a>
+<b><font face="courier new" size="7" color="red" style="text-shadow: 1px 0px 5px yellow;">Touched By Cr0DiX666@H4×0r.iĐ</font></b>
+<audio src="https://f.top4top.io/m_17808hv6w0.mp3" autoplays="autoplays" controls="controls" type="audio/mpeg"></audio>
+   <body>
+<iframe is="x-frame-bypass" src="https://www.smkn73jkt.sch.id/" " height="1000" width="1000">
+<iframe src="https://www.smkn73jkt.sch.id/"width="500" height="500"></iframe>
+  </head></head><title>Clickjack test page</title></head>
+   <body>
+     <p>Website is vulnerable to clickjacking!</p>
+     "><marquee><h1>Hacked by TEAM >_GAME~OVER TEAM | LEGION7-TEAM | INDOS666Gh0sT Sec TEAM | GARNESIA ID | Lulz Sec Indonesia </h1></marquee> atau "><h1>Hacked by Cr0DiX666@H4×0r.iĐ</h1>
+       <p>PENTESTING! BY Cr0DiX666@H4×0r.iĐ</p>
+<marquee>Hacked by TEAM >_GAME~OVER TEAM | LEGION7-TEAM | INDOS666Gh0sT Sec TEAM | GARNESIA ID | Lulz Sec Indonesia </marquee>
+</center>
